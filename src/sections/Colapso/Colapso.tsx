b3bf1c6740dacc34d0f61e4a9ea778efc33a5cbf
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2026-07-03 17:51:48 -0300

    add animation to colapso cards

--- src/sections/Colapso/Colapso.tsx
+++ src/sections/Colapso/Colapso.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { PROBLEM_CARDS, asset } from '../../data/content'
 import styles from './Colapso.module.css'
 
@@ -32,14 +33,25 @@ export function Colapso() {
           </p>
         </div>
         <div className={styles.grid}>
-          {PROBLEM_CARDS.map((card) => (
-            <article key={card.tag} className={styles.card}>
+          {PROBLEM_CARDS.map((card, index) => (
+            <motion.article
+              key={card.tag}
+              className={styles.card}
+              initial={{ scale: 0.7, opacity: 0 }}
+              whileInView={{ scale: 1, opacity: 1 }}
+              viewport={{ once: false, amount: 0.35 }}
+              transition={{
+                duration: 0.6,
+                delay: index * 0.15,
+                ease: [0.22, 1, 0.36, 1],
+              }}
+            >
               <img src={asset(card.image)} alt="" className={styles.image} loading="lazy" />
               <div className={styles.body}>
                 <span className={styles.tag}>{card.tag}</span>
                 <h3 className="h4">{card.stat}</h3>
               </div>
-            </article>
+            </motion.article>
           ))}
         </div>
       </div>
